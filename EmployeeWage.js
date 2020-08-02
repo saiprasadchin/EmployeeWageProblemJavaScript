91ff035f5cf555f20054a04cfe0348c61295d714
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-08-02 09:06:42 -0700

    Added : Code to calculate wage upto 100 hours or 20 days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -2,27 +2,28 @@ let EMP_RATE_PER_HOUR = 20;
 let IS_PART_TIME = 1;
 let IS_FULL_TIME = 2;
 let NUM_OF_WORKING_DAYS = 20;
+let MAX_HRS_IN_MONTH = 100;
 
-let isPresent = 1;
 let monthlyWage = 0;
+let empHrs = 0, empWage = 0;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
 
-let empHrs = 0;
-let empWage = 0;
-
-for ( let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-        let empCheck = Math.floor(Math.random() * 10) % 3;
-        switch(empCheck){
-            case IS_PART_TIME:
-                empHrs = 4;
-                break
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break
-            default:
-                empHrs = 0;
-        }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        monthlyWage += empWage;
-
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    switch(empCheck) {
+    case IS_PART_TIME:
+        empHrs = 4;
+        break
+    case IS_FULL_TIME:
+        empHrs = 8;
+        break
+    default:
+        empHrs = 0;
+    }
+    totalEmpHrs += empHrs;
 }
-console.log("Employee Montly Wage = "+monthlyWage);
+
+monthlyWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+console.log("Employee Monthly Wage = " + monthlyWage);
