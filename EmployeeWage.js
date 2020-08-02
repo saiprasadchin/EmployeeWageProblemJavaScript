28f38a781dad008df8a5bf30eb3a3cfcf25c12b2
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-08-02 08:37:55 -0700

    Added : Code to calculate wage for part time and full time employee

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,12 @@
 let EMP_RATE_PER_HOUR = 20;
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
 
-let isPresent = 1
 let empHrs = 0, empWage = 0;
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == isPresent)
+let empCheck = Math.floor(Math.random() * 10) % 3;
+if (empCheck == IS_PART_TIME)
+    empHrs = 4;
+else if (empCheck == IS_FULL_TIME)
     empHrs = 8;
 else
     empHrs = 0;
