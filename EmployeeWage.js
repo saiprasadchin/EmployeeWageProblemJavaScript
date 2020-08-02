7c137f5ae12ed8b4d4feb61b9816dbdcb1e1dedd
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-08-02 08:49:32 -0700

    Added : Code to calculate wage of part and full time employee using switch case

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,12 +3,18 @@ let IS_PART_TIME = 1;
 let IS_FULL_TIME = 2;
 
 let empHrs = 0, empWage = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-if (empCheck == IS_PART_TIME)
-    empHrs = 4;
-else if (empCheck == IS_FULL_TIME)
-    empHrs = 8;
-else
-    empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3
+
+switch(empCheck){
+    case IS_PART_TIME:
+        empHrs = 4;
+        break
+    case IS_FULL_TIME:
+        empHrs = 8;
+        break
+    default:
+        empHrs = 0;
+}
+
 empWage = empHrs * EMP_RATE_PER_HOUR;
-console.log("Employee Wage : " +empWage);
+console.log("Employee Wage = "+empWage);
